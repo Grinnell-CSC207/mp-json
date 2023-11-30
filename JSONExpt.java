6e2a91b6b5969bf87c5f6a7f08825e336c278fa3
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-11-30 17:16:45 -0600

    Update JSONExpt.java

--- JSONExpt.java
+++ JSONExpt.java
@@ -1,27 +1,24 @@
 import java.io.IOException;
 import java.text.ParseException;
 
+/**
+ * Test JSON parsing with a sample JSON string.
+ */
 public class JSONExpt {
     public static void main(String[] args) {
         // Test JSON string
-        String jsonString = "{ \"a\": \"apple\"}";
-  
-        // Print the characters in the string
-        for (int i = 0; i < jsonString.length(); i++) {
-            System.out.println("Char at index " + i + ": " + jsonString.charAt(i));
-        }
+        String sampleJsonString = "{ \"a\": \"apple\", \"e\": [2,7,1,8], \"q\": { \"x\" : \"xerox\" } }";
   
         try {
             // Parse JSON string
-            JSONValue jsonValue = JSON.parse(jsonString);
+            JSONValue jsonValue = JSON.parse(sampleJsonString);
   
             // Display the parsed JSON value
             System.out.println("Parsed JSON Value:");
             System.out.println(jsonValue);
   
         } catch (ParseException | IOException e) {
-            e.printStackTrace();
+            System.err.println("Error parsing JSON: " + e.getMessage());
         }
     }
-  }
-  
+}
